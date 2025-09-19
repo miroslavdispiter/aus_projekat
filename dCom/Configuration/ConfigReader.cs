d38df33a2c35cce6dc5a05fd8b5dbb46759a0eba
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-09-19 13:49:23 +0200

    Konfiguracija

--- dCom/Configuration/ConfigReader.cs
+++ dCom/Configuration/ConfigReader.cs
@@ -9,181 +9,195 @@ using System.Linq;
 namespace dCom.Configuration
 {
     internal class ConfigReader : IConfiguration
-	{
-		private ushort transactionId = 0;
+    {
+        private ushort transactionId = 0;
 
-		private byte unitAddress;
-		private int tcpPort;
-		private ConfigItemEqualityComparer confItemEqComp = new ConfigItemEqualityComparer();
+        private byte unitAddress;
+        private int tcpPort;
+        private ConfigItemEqualityComparer confItemEqComp = new ConfigItemEqualityComparer();
 
-		private Dictionary<string, IConfigItem> pointTypeToConfiguration = new Dictionary<string, IConfigItem>();
+        private Dictionary<string, IConfigItem> pointTypeToConfiguration = new Dictionary<string, IConfigItem>();
 
-		private string path = "RtuCfg.txt";
+        private string path = "RtuCfg.txt";
 
-		public ConfigReader()
-		{
-			if (!File.Exists(path))
-			{
-				OpenConfigFile();
-			}
+        public ConfigReader()
+        {
+            if (!File.Exists(path))
+            {
+                OpenConfigFile();
+            }
 
-			ReadConfiguration();
-		}
+            ReadConfiguration();
+        }
 
-		public int GetAcquisitionInterval(string pointDescription)
-		{
-			IConfigItem ci;
-			if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
-			{
-				return ci.AcquisitionInterval;
-			}
-			throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
-		}
+        public int GetAcquisitionInterval(string pointDescription)
+        {
+            IConfigItem ci;
+            if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
+            {
+                return ci.AcquisitionInterval;
+            }
+            throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
+        }
 
-		public ushort GetStartAddress(string pointDescription)
-		{
-			IConfigItem ci;
-			if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
-			{
-				return ci.StartAddress;
-			}
-			throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
-		}
+        public ushort GetStartAddress(string pointDescription)
+        {
+            IConfigItem ci;
+            if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
+            {
+                return ci.StartAddress;
+            }
+            throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
+        }
 
-		public ushort GetNumberOfRegisters(string pointDescription)
-		{
-			IConfigItem ci;
-			if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
-			{
-				return ci.NumberOfRegisters;
-			}
-			throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
-		}
+        public ushort GetNumberOfRegisters(string pointDescription)
+        {
+            IConfigItem ci;
+            if (pointTypeToConfiguration.TryGetValue(pointDescription, out ci))
+            {
+                return ci.NumberOfRegisters;
+            }
+            throw new ArgumentException(string.Format("Invalid argument:{0}", nameof(pointDescription)));
+        }
 
-		private void OpenConfigFile()
-		{
-			OpenFileDialog dlg = new OpenFileDialog();
-			dlg.Multiselect = false;
-			dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-			dlg.FileOk += Dlg_FileOk;
-			dlg.ShowDialog();
-		}
+        private void OpenConfigFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = false;
+            dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            dlg.FileOk += Dlg_FileOk;
+            dlg.ShowDialog();
+        }
 
-		private void Dlg_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
-		{
-			path = (sender as OpenFileDialog).FileName;
-		}
+        private void Dlg_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            path = (sender as OpenFileDialog).FileName;
+        }
 
-		private void ReadConfiguration()
-		{
-			using (TextReader tr = new StreamReader(path))
-			{
-				string s = string.Empty;
-				while ((s = tr.ReadLine()) != null)
-				{
-					string[] splited = s.Split(' ', '\t');
-					List<string> filtered = splited.ToList().FindAll(t => !string.IsNullOrEmpty(t));
-					if (filtered.Count == 0)
-					{
-						continue;
-					}
-					if (s.StartsWith("STA"))
-					{
-						unitAddress = Convert.ToByte(filtered[filtered.Count - 1]);
-						continue;
-					}
-					if (s.StartsWith("TCP"))
-					{
-						TcpPort = Convert.ToInt32(filtered[filtered.Count - 1]);
-						continue;
-					}
-					if (s.StartsWith("DBC"))
-					{
-						DelayBetweenCommands = Convert.ToInt32(filtered[filtered.Count - 1]);
-						continue;
-					}
-					try
+        private void ReadConfiguration()
+        {
+            using (TextReader tr = new StreamReader(path))
+            {
+                string s = string.Empty;
+                while ((s = tr.ReadLine()) != null)
+                {
+                    string[] splited = s.Split(' ', '\t');
+                    List<string> filtered = splited.ToList().FindAll(t => !string.IsNullOrEmpty(t));
+                    if (filtered.Count == 0) continue;
+
+                    if (s.StartsWith("STA"))
                     {
-                        ConfigItem ci = new ConfigItem(filtered);
-                        if (pointTypeToConfiguration.Count > 0)
+                        unitAddress = Convert.ToByte(filtered[filtered.Count - 1]);
+                        continue;
+                    }
+                    if (s.StartsWith("TCP"))
+                    {
+                        TcpPort = Convert.ToInt32(filtered[filtered.Count - 1]);
+                        continue;
+                    }
+                    if (s.StartsWith("DBC"))
+                    {
+                        DelayBetweenCommands = Convert.ToInt32(filtered[filtered.Count - 1]);
+                        continue;
+                    }
+
+                    try
+                    {
+                        ConfigItem tempCi = new ConfigItem(filtered);
+
+                        if (tempCi.NumberOfRegisters > 1)
                         {
-                            foreach (ConfigItem cf in pointTypeToConfiguration.Values)
+                            for (int i = 0; i < tempCi.NumberOfRegisters; i++)
                             {
-                                if (!confItemEqComp.Equals(cf, ci))
+                                List<string> dupParams = new List<string>(filtered);
+
+                                dupParams[1] = "1";
+                                dupParams[2] = (tempCi.StartAddress + i).ToString();
+
+                                string baseDesc = filtered[8].TrimStart('@');
+                                dupParams[8] = "@" + baseDesc + (i + 1);
+
+                                ConfigItem ci = new ConfigItem(dupParams);
+
+                                if (!pointTypeToConfiguration.ContainsKey(ci.Description))
                                 {
                                     pointTypeToConfiguration.Add(ci.Description, ci);
-                                    break;
                                 }
                             }
                         }
                         else
                         {
-                            pointTypeToConfiguration.Add(ci.Description, ci);
+                            ConfigItem ci = new ConfigItem(filtered);
+                            if (!pointTypeToConfiguration.ContainsKey(ci.Description))
+                            {
+                                pointTypeToConfiguration.Add(ci.Description, ci);
+                            }
                         }
                     }
                     catch (ArgumentException argEx)
                     {
                         throw new ConfigurationException($"Configuration error: {argEx.Message}", argEx);
                     }
-                    catch (Exception ex)
+                    catch (Exception)
                     {
-                        throw ex;
+                        throw;
                     }
                 }
-				if (pointTypeToConfiguration.Count == 0)
-				{
-					throw new ConfigurationException("Configuration error! Check RtuCfg.txt file!");
-				}
-			}
-		}
 
-		public ushort GetTransactionId()
-		{
-			return transactionId++;
-		}
+                if (pointTypeToConfiguration.Count == 0)
+                {
+                    throw new ConfigurationException("Configuration error! Check RtuCfg.txt file!");
+                }
+            }
+        }
 
-		public byte UnitAddress
-		{
-			get
-			{
-				return unitAddress;
-			}
+        public ushort GetTransactionId()
+        {
+            return transactionId++;
+        }
 
-			private set
-			{
-				unitAddress = value;
-			}
-		}
+        public byte UnitAddress
+        {
+            get
+            {
+                return unitAddress;
+            }
 
-		public int TcpPort
-		{
-			get
-			{
-				return tcpPort;
-			}
+            private set
+            {
+                unitAddress = value;
+            }
+        }
 
-			private set
-			{
-				tcpPort = value;
-			}
-		}
-		private int dbc;
-		public int DelayBetweenCommands
-		{
-			get
-			{
-				return dbc;
-			}
+        public int TcpPort
+        {
+            get
+            {
+                return tcpPort;
+            }
 
-			private set
-			{
-				dbc = value;
-			}
-		}
+            private set
+            {
+                tcpPort = value;
+            }
+        }
+        private int dbc;
+        public int DelayBetweenCommands
+        {
+            get
+            {
+                return dbc;
+            }
 
-		public List<IConfigItem> GetConfigurationItems()
-		{
-			return new List<IConfigItem>(pointTypeToConfiguration.Values);
-		}
-	}
+            private set
+            {
+                dbc = value;
+            }
+        }
+
+        public List<IConfigItem> GetConfigurationItems()
+        {
+            return new List<IConfigItem>(pointTypeToConfiguration.Values);
+        }
+    }
 }
