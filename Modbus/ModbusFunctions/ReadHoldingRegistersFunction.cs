93883db18f53a68714d722dfa1663522b0089255
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-09-19 14:03:49 +0200

    ReadHoldingRegistersFunction implementirana

--- Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -24,15 +24,42 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters cp = this.CommandParameters as ModbusReadCommandParameters;
+
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)cp.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)cp.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)cp.Length)), 0, request, 4, 2);
+
+            request[6] = cp.UnitId;
+            request[7] = cp.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)cp.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)cp.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters cp = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort address = cp.StartAddress;
+
+            for (int i = 0; i < response[8] / 2; i++)
+            {
+                byte byte1 = response[9 + i * 2];
+                byte byte2 = response[9 + i * 2 + 1];
+                ushort value = BitConverter.ToUInt16(new byte[2] { byte2, byte1 }, 0);
+
+                result.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), value);
+                address++;
+            }
+
+            return result;
         }
     }
 }
